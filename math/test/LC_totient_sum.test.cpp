7d91773f683374632c36b9292a0ad1dfc952b5fb
hotman <hotmanww@gmail.com>
2020-09-13 15:47:08 +0900

    add totient_sum test

---
+++ math/test/LC_totient_sum.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_totient_function"
+#include "../totient_sum.hpp"
+#include "../mod_int998244353.hpp"
+#include "../../util/template.hpp"
+
+int main(){
+    lint n;
+    cin>>n;
+    cout<<totient_sum<mint>(n)<<endl;
+}
