78de3410fc54405da4df4f433d5803c0c79113fe
hotmanww <hotmanww@gmail.com>
2023-02-05 08:58:51 +0900

    segtreeをmaybeを参照しないように変更

--- segment_tree/segment_tree.hpp
+++ segment_tree/segment_tree.hpp
@@ -1,105 +1,135 @@
 #pragma once
-#include<vector>
-#include"../alga/maybe.hpp"
 
 /**
  * @brief セグメント木
  * @see https://en.wikipedia.org/wiki/Segment_tree
  */
 
-template<typename T,typename F>
-class segment_tree{
-    using i64=long long;
-    struct node;
-    using np=node*;
-    struct node{
-        maybe<T>val;
-        np ch[2]={nullptr,nullptr};
-        node(maybe<T> val=maybe<T>()):val(val){}
-    };
-    np root=new node();
-    i64 n=1;
-    i64 sz;
-    F op;
-	public:
-    segment_tree(){}
-	segment_tree(i64 sz,F op=F()):sz(sz),op(op){
-		while(n<=sz)n<<=1;
-	}
-    segment_tree(std::vector<T> v,F op=F()):sz(v.size()),op(op){
-		while(n<=sz)n<<=1;
-        for(int i=0;i<sz;++i)change(i,v[i]);
-	}
-    maybe<T> get(i64 a,i64 b){
-        return get(a,b,0,n,root);
-    }
-    void apply(i64 x,T val){
-        apply(x,val,0,n,root);
-    }
-    void apply_left(i64 x,T val){
-        apply_left(x,val,0,n,root);
-    }
-    void change(i64 x,T val){
-        change(x,val,0,n,root);
-    }
-    maybe<T> get(i64 a,i64 b,i64 l,i64 r,np t){
-        if(!t)return maybe<T>();
-        auto f=expand<T,F>(op);
-        if(r<=a||b<=l)return maybe<T>();
-        if(a<=l&&r<=b){
-            return t->val;
+namespace hotman{
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    template<typename T,typename F>
+    class segment_tree{
+        using i64=long long;
+        using u8=unsigned char;
+        struct node;
+        using np=node*;
+        struct node{
+            optional<T>val;
+            np ch[2]={nullptr,nullptr};
+            node(optional<T> val=nullopt):val(val){}
+        };
+        np root=new node();
+        i64 n=1;
+        i64 sz;
+        F op;
+        public:
+        segment_tree(){}
+        segment_tree(i64 sz,F op=F()):sz(sz),op(op){
+            while(n<=sz)n<<=1;
         }
-        const i64 m=(l+r)/2;
-        return f(get(a,b,l,m,t->ch[0]),get(a,b,m,r,t->ch[1]));
-    }
-    void apply(i64 x,T val,i64 l,i64 r,np t){
-        auto f=expand<T,F>(op);
-        if(!t->ch[0])t->ch[0]=new node();
-        if(!t->ch[1])t->ch[1]=new node();
-        if(l<=x&&x<r){
-            t->val=f(t->val,val);
+        segment_tree(std::vector<T> v,F op=F()):sz(v.size()),op(op){
+            while(n<=sz)n<<=1;
+            for(int i=0;i<sz;++i)change(i,v[i]);
+        }
+        optional<T> get(i64 a,i64 b){
+            return get(a,b,0,n,root);
+        }
+        void apply(i64 x,T val){
+            apply(x,val,0,n,root);
+        }
+        void apply_left(i64 x,T val){
+            apply_left(x,val,0,n,root);
+        }
+        void change(i64 x,T val){
+            change(x,val,0,n,root);
+        }
+        optional<T> get(i64 a,i64 b,i64 l,i64 r,np t){
+            if(!t)return nullopt;
+            if(r<=a||b<=l)return nullopt;
+            if(a<=l&&r<=b){
+                return t->val;
+            }
+            const i64 m=(l+r)/2;
+            return __op(get(a,b,l,m,t->ch[0]),get(a,b,m,r,t->ch[1]));
+        }
+        optional<T> apply(i64 x,T val,i64 l,i64 r,np t){
             const i64 m=(l+r)/2;
             if(r-l>1){
-                apply(x,val,l,m,t->ch[0]);
-                apply(x,val,m,r,t->ch[1]);
+                if(l<=x&&x<m){
+                    if(!t->ch[0])t->ch[0]=new node();
+                    apply(x,val,l,m,t->ch[0]);
+                }
+                if(m<=x&&x<r){
+                    if(!t->ch[1])t->ch[1]=new node();
+                    apply(x,val,m,r,t->ch[1]);
+                }
+                return t->val=__op(t->ch[0]?t->ch[0]->val:nullopt,t->ch[1]?t->ch[1]->val:nullopt);
+            }else{
+                return t->val=__op(t->val,val);
             }
         }
-    }
-    void apply_left(i64 x,T val,i64 l,i64 r,np t){
-        if(!t->ch[0])t->ch[0]=new node();
-        if(!t->ch[1])t->ch[1]=new node();
-        auto f=expand<T,F>(op);
-        if(l<=x&&x<r){
-            t->val=f(val,t->val);
-            i64 m=(l+r)/2;
+        optional<T> apply_left(i64 x,T val,i64 l,i64 r,np t){
+            const i64 m=(l+r)/2;
             if(r-l>1){
-                apply_left(x,val,l,m,t->ch[0]);
-                apply_left(x,val,m,r,t->ch[1]);
+                if(l<=x&&x<m){
+                    if(!t->ch[0])t->ch[0]=new node();
+                    apply_left(x,val,l,m,t->ch[0]);
+                }
+                if(m<=x&&x<r){
+                    if(!t->ch[1])t->ch[1]=new node();
+                    apply_left(x,val,m,r,t->ch[1]);
+                }
+                return t->val=__op(t->ch[0]?t->ch[0]->val:nullopt,t->ch[1]?t->ch[1]->val:nullopt);
+            }else{
+                return t->val=__op(val,t->val);
             }
         }
-    }
-    maybe<T> change(i64 x,const T& val,i64 l,i64 r,np t){
-        if(!t->ch[0])t->ch[0]=new node();
-        if(!t->ch[1])t->ch[1]=new node();
-        auto f=expand<T,F>(op);
-        if(l<=x&&x<r){
+        optional<T> change(i64 x,const T& val,i64 l,i64 r,np t){
+            const i64 m=(l+r)/2;
             if(r-l>1){
-                const i64 m=(l+r)/2;
-                auto p=change(x,val,l,m,t->ch[0]);
-                auto q=change(x,val,m,r,t->ch[1]);
-                return t->val=f(p,q);
+                if(l<=x&&x<m){
+                    if(!t->ch[0])t->ch[0]=new node();
+                    change(x,val,l,m,t->ch[0]);
+                }
+                if(m<=x&&x<r){
+                    if(!t->ch[1])t->ch[1]=new node();
+                    change(x,val,m,r,t->ch[1]);
+                }
+                return t->val=__op(t->ch[0]?t->ch[0]->val:nullopt,t->ch[1]?t->ch[1]->val:nullopt);
             }else{
                 return t->val=val;
             }
         }
-        return t->val;
-    }
-};
+        optional<T> __op(const optional<T>& s,const optional<T>& t){
+            if(!s.has_value())return t;
+            if(!t.has_value())return s;
+            return op(s.value(),t.value());
+        }
+    };
 
-template<typename T>
-struct MAX_F{
-    T operator()(const T& s,const T& t){
-        return max(s,t);
-    }
-};
-template<typename T>using max_seg=segment_tree<T,MAX_F<T>>;
+    template<typename T>
+    struct MAX_F{
+        T operator()(const T& s,const T& t){
+            return max(s,t);
+        }
+    };
+    template<typename T>using max_seg=segment_tree<T,MAX_F<T>>;
+
+    template<typename T>
+    struct MIN_F{
+        T operator()(const T& s,const T& t){
+            return min(s,t);
+        }
+    };
+    template<typename T>using min_seg=segment_tree<T,MIN_F<T>>;
+    
+    template<typename T>
+    struct COMPOSITE_F{
+        pair<T,T> operator()(const pair<T,T>& s,const pair<T,T>& t){
+            return make_pair(s.first*t.first,s.second*t.first+t.second);
+        }
+    };
+    template<typename T>using composite_seg=segment_tree<pair<T,T>,COMPOSITE_F<T>>;
+}
